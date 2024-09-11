d9663a50dcb7d54b29fc98e372df4d14f436c668
karachy00 <kabylbek470@gmail.com>
2024-09-11 14:27:59 +0600

    menu-html,js

--- menu.js
+++ menu.js
@@ -14,7 +14,7 @@ const addBtn = document.querySelector('.addBtn')
 const saveBtn = document.querySelector('.saveBtn')
 const removeAll = document.querySelector('.removeAll')
 const removeBtn = document.querySelector('.removeBtn')
-const order = document.querySelector('.order')
+
 menuA.addEventListener('click' , () => {
     menuId.style.display = 'flex'
     orderId.style.display = 'none'
@@ -25,12 +25,14 @@ orderA.addEventListener('click' , () => {
     orderId.style.display = 'flex'
     menuId.style.display = 'none'
     adminId.style.display = 'none'
+   
+
 
 })
 adminA.addEventListener('click' , () => {
     adminId.style.display = 'flex'
-    // orderId.style.display = 'none'
     menuId.style.display = 'none'
+    orderId.style.display = 'none'
 
 })
 
@@ -51,16 +53,19 @@ function createData(){
         name:inName.value,
         img:inProd.value,
         age:inPrice.value,
+        count: 1 ,
     }
     let storage = JSON.parse(localStorage.getItem('card')) || [];
-    // if(
-    //     storage.some((el) => el.inProd === newObj.inProd  ||  el.inName == newObj.inName)
-    //   )  {
-    //     alert('Такой продукт уже есть');
-        
-    //     return;
+    if(
+        storage.some((el) => el.img === newObj.img  ||  el.name == newObj.name)
+      )  {
+        alert('Такой продукт уже есть');
+        for(let inp of input ){
+            inp.value = '';
+        }
+        return;
     
-    // }
+    }
     if(!inName.value && !inProd.value && !inPrice.value){
       alert('Заполните все поле')
       return;
@@ -112,38 +117,39 @@ storage.forEach((el , idx) => {
     menutextDiv.append(h2)
     div.append(span)
     menutextDiv.append(btnDiv)
-   
+    btnDiv.append(basBtn)
     btnDiv.append(delBtn)
     btnDiv.append(editBtn)
-    btnDiv.append(basBtn)
 
 
     img.src = el.img;
     h2.innerText = el.name;
-    span.innerText = el.age  ;
-    editBtn.innerHTML = '<ion-icon name="pencil-outline"></ion-icon>'
-    editBtn.addEventListener('click' , () =>  {
+    span.innerText = `${el.age}$`;
+
+//////////////////////////////////////////////////////// EDTIT CLICK ///////////////////////////////////////////////////////////////////////////////////////
+
+editBtn.innerHTML = '<ion-icon name="create-outline"></ion-icon>'
+editBtn.addEventListener('click' , () =>  {
         editData(idx)
     })
-    delBtn.innerHTML = '<ion-icon name="trash-outline"></ion-icon>'
-    delBtn.addEventListener('click' , () => delData(idx))
-})
-}
-    basBtn.innerHTML = '<ion-icon name="cart-outline"></ion-icon>'
-    basBtn.addEventListener('click' , () => {
-        basketData(el)
-    })
-  
-    function basketData(newObj){
-   
-        let basket = JSON.parse(localStorage.getItem("basket")) || [];
-        basket.push(newObj);
-        localStorage.setItem("basket", JSON.stringify(basket));
 
-         getBasket();
-      
+/////////////////////////////////////////////////////////////////// BASKET  CLICK //////////////////////////////////////////////////////////////////////////////////
+
+basBtn.innerHTML = '<ion-icon name="cart-outline"></ion-icon>'
+basBtn.addEventListener('click' , () => {
+    basketData(el)
+})
+
+/////////////////////////////////////////////////////////////////// DELETE CLICK ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+delBtn.innerHTML = '<ion-icon name="trash-outline"></ion-icon>'
+delBtn.addEventListener('click' , () => delData(idx))
+
+})
+
 }
   
+
     
 /////////////////////////////////////////////////////////////// DElETE/////////////////////////////////////////////////////////////////////////
 
@@ -166,7 +172,7 @@ removeBtn.addEventListener('click' , ()  => {
 
 
 function editData (ind){
-let storage = JSON.parse(localStorage.getItem('card')) || [];
+ let storage = JSON.parse(localStorage.getItem('card')) || [];
  let onProd  = storage.splice(ind , 1)[0];
  inProd.value = onProd.img
  inName.value = onProd.name;
@@ -208,76 +214,247 @@ for (let inp of input) {
 
 ////////////////////////////////////////////////////////////////// BASKET ///////////////////////////////////////////////////////////////////////
 
+function basketData(newObj) {
+    let basket = JSON.parse(localStorage.getItem("basket")) || [];
+    basket.push(newObj);
+    localStorage.setItem("basket", JSON.stringify(basket));
 
+    getBasket();
+}
 
-
+const orderBas = document.querySelector('.orderBas');
+getBasket()
 function getBasket() {
-      basket.innerHTML = "";
-      let basket = JSON.parse(localStorage.getItem("basket")) || [];
-      basket.forEach((el, index) => {
-      let orderBlock = document.createElement("div");
-      orderBlock.classList.add("orderBlock");
-      let orderBtns = document.createElement("div");
-      orderBtns.classList.add("orderBtns");
-      let orderImg = document.createElement("img");
-      orderImg.classList.add("orderImg");
-      let orderText = document.createElement("div");
-      orderText.classList.add("orderText");
-  
-      let orderName = document.createElement("h3");
-      orderName.classList.add("orderName");
-      let orderPrice = document.createElement("h4");
-      orderPrice.classList.add("orderPrice");
-      let orderCount = document.createElement("div");
-      orderCount.classList.add("orderCount");
-      let num = document.createElement("h2");
-  
-      let orderDelete = document.createElement("button");
-      orderDelete.classList.add("orderDelete");
-      let orderPlus = document.createElement("button");
-      orderPlus.classList.add("orderPlus");
-      let count = document.createElement("h2");
-      count.classList.add("count");
-      let orderMinus = document.createElement("button");
-      orderMinus.classList.add("orderMinus");
-  
-      orderImg.src = el.inProd;
-      orderName.innerText = el.inName;
-      num = el.count;
-      orderPrice.innerHTML = el.count * el.inPrice;
-  
-      orderDelete.innerText = "delete order";
-      orderPlus.innerText = "+";
-      orderMinus.innerText = "-";
-      num.innerHTML = el.count;
-      // count.innerHTML = `${num}x`;
-  
-      orderBlock.append(orderImg);
-      orderBlock.append(orderText);
-      orderBlock.append(orderBtns);
-  
-      orderText.append(orderName);
-      orderText.append(orderPrice);
-  
-      orderBtns.append(orderDelete);
-      orderBtns.append(orderCount);
-  
-      orderCount.append(orderMinus);
-      orderCount.append(num);
-      orderCount.append(orderPlus);
-      basket.append(orderBlock);
-      console.log(el);
-  
-      orderDelete.addEventListener("click", () => {
-        delOrder(index);
-        getBasket();
-      });
-  
-      orderPlus.addEventListener("click", () => {
-        num += 1;
-      });
-      orderMinus.addEventListener("click", () => {
-        num -= 1;
-      });
+    orderBas.innerHTML = "";
+    let basket = JSON.parse(localStorage.getItem("basket")) || [];
+    basket.forEach((el, index) => {
+        let orderBlock = document.createElement("div");
+        orderBlock.classList.add("orderBlock");
+        let orderBtns = document.createElement("div");
+        orderBtns.classList.add("orderBtns");
+        let orderImg = document.createElement("img");
+        orderImg.classList.add("orderImg");
+        let orderText = document.createElement("div");
+        orderText.classList.add("orderText");
+
+        let orderName = document.createElement("h3");
+        orderName.classList.add("orderName");
+        let orderPrice = document.createElement("h4");
+        orderPrice.classList.add("orderPrice");
+        let orderCount = document.createElement("div");
+        orderCount.classList.add("orderCount");
+        let count = document.createElement("h2");
+        count.classList.add("count");
+
+        let orderDelete = document.createElement("button");
+        orderDelete.classList.add("orderDelete");
+        let orderPlus = document.createElement("button");
+        orderPlus.classList.add("orderPlus");
+        let orderMinus = document.createElement("button");
+        orderMinus.classList.add("orderMinus");
+
+        orderImg.src = el.img;
+        orderName.innerText = el.name;
+        count.innerText = el.count;
+        orderPrice.innerHTML = el.count * el.age;
+
+        orderDelete.innerText = "delete";
+        orderPlus.innerText = "+ ";
+        orderMinus.innerText = "-";
+
+        orderBas.append(orderBlock);
+        orderBlock.append(orderImg);
+        orderBlock.append(orderText);
+        orderBlock.append(orderBtns);
+
+        orderText.append(orderName);
+        orderText.append(orderPrice);
+
+        orderBtns.append(orderDelete);
+        orderBtns.append(orderCount);
+
+        orderCount.append(orderMinus);
+        orderCount.append(count);
+        orderCount.append(orderPlus);
+
+        orderDelete.addEventListener("click", () => {
+            delOrder(index);
+            getBasket();
+        });
+
+        orderPlus.addEventListener("click", () => {
+            el.count += 1;
+            count.innerHTML = el.count;
+            orderPrice.innerHTML = el.count * el.age;
+            localStorage.setItem("basket", JSON.stringify(basket));
+        });
+
+        orderMinus.addEventListener("click", () => {
+            if (el.count > 0) {
+                el.count -= 1;
+                count.innerHTML = el.count;
+                orderPrice.innerHTML = el.count * el.age;
+                localStorage.setItem("basket", JSON.stringify(basket));
+            }
+        });
     });
-  }
+}
+
+// Определите функцию delOrder здесь
+function delOrder(index) {
+    let basket = JSON.parse(localStorage.getItem("basket")) || [];
+    basket.splice(index, 1);
+    localStorage.setItem("basket", JSON.stringify(basket));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function deleteProduct(index) {
+//     let data = JSON.parse(localStorage.getItem("food")) || [];
+//     data.splice(index, 1);
+//     localStorage.setItem("food", JSON.stringify(data));
+//     newProduct();
+//   } 
+  
+// function basketData(newObj){
+   
+//     let basket = JSON.parse(localStorage.getItem("basket")) || [];
+//     basket.push(newObj);
+//     localStorage.setItem("basket", JSON.stringify(basket));
+
+//      getBasket();
+  
+// }
+
+// const orderBas = document.querySelector('.orderBas')
+// basketData()
+// function getBasket() {
+//       orderBas.innerHTML = "";
+//       let basket = JSON.parse(localStorage.getItem("basket")) || [];
+//       basket.forEach((el, index) => {
+//       let orderBlock = document.createElement("div");
+//       orderBlock.classList.add("orderBlock");
+//       let orderBtns = document.createElement("div");
+//       orderBtns.classList.add("orderBtns");
+//       let orderImg = document.createElement("img");
+//       orderImg.classList.add("orderImg");
+//       let orderText = document.createElement("div");
+//       orderText.classList.add("orderText");
+  
+//       let orderName = document.createElement("h3");
+//       orderName.classList.add("orderName");
+//       let orderPrice = document.createElement("h4");
+//       orderPrice.classList.add("orderPrice");
+//       let orderCount = document.createElement("div");
+//       orderCount.classList.add("orderCount");
+//       let num = document.createElement("h2");
+  
+//       let orderDelete = document.createElement("button");
+//       orderDelete.classList.add("orderDelete");
+//       let orderPlus = document.createElement("button");
+//       orderPlus.classList.add("orderPlus");
+//       let count = document.createElement("h2");
+//       count.classList.add("count");
+//       let orderMinus = document.createElement("button");
+//       orderMinus.classList.add("orderMinus");
+  
+//       orderImg.src = el.inProd;
+//       orderName.innerText = el.inName;
+//       num = el.count;
+//       orderPrice.innerHTML = el.count * el.inPrice;
+  
+//       orderDelete.innerText = "delete order";
+//       orderPlus.innerText = "+";
+//       orderMinus.innerText = "-";
+//       num.innerHTML = el.count;
+     
+//        orderBas.append(orderBlock)
+//       orderBlock.append(orderImg);
+//       orderBlock.append(orderText);
+//       orderBlock.append(orderBtns);
+  
+//       orderText.append(orderName);
+//       orderText.append(orderPrice);
+  
+//       orderBtns.append(orderDelete);
+//       orderBtns.append(orderCount);
+  
+//       orderCount.append(orderMinus);
+//       orderCount.append(num);
+//       orderCount.append(orderPlus);
+//       console.log(el);
+  
+//       orderDelete.addEventListener("click", () => {
+//         delOrder(index);
+//         getBasket();
+//       });
+  
+//       orderPlus.addEventListener("click", () => {
+//         num += 1;
+//       });
+//       orderMinus.addEventListener("click", () => {
+//         num -= 1;
+//       });
+//     });
+    
+//   }
